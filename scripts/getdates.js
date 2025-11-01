f7131a5b71427ec074c8b2933d02eba0668049a6
Ygor Castro <ygor.castro@outlook.com.br>
2025-11-01 20:03:04 -0300

    WDD 131 – Dynamic Web Fundamentals - W01
    WDD 131 – Dynamic Web Fundamentals - W01

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
